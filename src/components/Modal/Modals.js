53184d049517b6b9af91c5e9c93671d7712762db
Beenyaa <bencegadanyi1@hotmail.com>
2021-03-02 23:32:00 +0000

    FIX: contact function

--- src/components/Modal/Modals.js
+++ src/components/Modal/Modals.js
@@ -37,22 +37,47 @@ export const AboutMeModal = (props) => {
 }
 
 export const ContactMeModal = (props) => {
+  const [formState, setFormState] = useState({
+    name:"",
+    email:"",
+    message:"",
+  })
+
+  const encode = (data) => {
+    return Object.keys(data)
+        .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+        .join("&");
+  }
+
+  const handleSubmit = e => {
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "contact", ...formState })
+    })
+      .then(() => console.log("Success!"))
+      .catch(error => console.log(error));
+
+    e.preventDefault();
+  };
+
+  const handleChange = e => setFormState({ ...formState, [e.target.name]: e.target.value });
 
   return {
     children: (
       <>
-        <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit="submit" action="/">
+        <form name="contact" method="POST" data-netlify="true" data-netlify-honeypot="bot-field" onSubmit={handleSubmit}>
         <p>
           <label for="name">Name</label>
-          <input type="text" id="name" name="name" required/>
+          <input type="text" id="name" name="name" value={formState.name} onChange={handleChange} required/>
         </p>
         <p>
           <label for="email">Email</label>
-          <input type="text" id="email" name="email" required/>
+          <input type="text" id="email" name="email" value={formState.email} onChange={handleChange} required/>
         </p>
         <p>
           <label for="message">Message</label>
-          <textarea id="message" name="message" required></textarea>
+          <textarea id="message" name="message" value={formState.message} onChange={handleChange} required></textarea>
         </p>
         <p>
           <button class="submit" type="submit"><b>Send</b></button>
