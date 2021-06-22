7bafd3698d92510b97956f91fa949c3bfac98528
Beenyaa <bencegadanyi1@hotmail.com>
2021-06-22 23:11:56 +0100

    seo changes x2

--- src/components/Seo.js
+++ src/components/Seo.js
@@ -5,23 +5,31 @@ const SEO = () => {
 return(
     //primary meta tags
     <Helmet>
-      <title content="Bence Gadányi"></title>
-      <meta name="title" content="Bence Gadányi's Portfolio"/>
-      <meta name="description" content="Bence Gadányi - Aspiring full-stack software engineer, from Hungary based in the UK. • Bence Gadanyi • Bence Gadányi • Gadányi Bence" />
-      <meta name="image" content="/static/bence_animoji.png" />
-      <meta name="author" content="Bence Gadányi"/>
+<title>Bence Gadányi</title>
+<meta name="title" content="Bence Gadányi"/>
+<meta name="description" content="Bence Gadányi - Aspiring full-stack software engineer, from Hungary based in the UK. 
+• Bence Gadanyi 
+• Bence Gadányi 
+• Gadányi Bence"/>
 
-      <meta name="og:title" content="Bence Gadányi's Portfolio"/>
-      <meta property="og:url" content="https://bence.codes" />
-      <meta property="og:type" content="website" />
-      <meta property="og:title" content="Bence Gadányi's Portfolio" />
-      <meta property="og:description" content="Bence Gadányi - Aspiring full-stack software engineer, from Hungary based in the UK. • Bence Gadanyi • Bence Gadányi • Gadányi Bence" />
-      <meta property="og:image" content="/static/bence_animoji.png" />
+<meta property="og:type" content="website"/>
+<meta property="og:url" content="https://bence.codes/"/>
+<meta property="og:title" content="Bence Gadányi"/>
+<meta property="og:description" content="Bence Gadányi - Aspiring full-stack software engineer, from Hungary based in the UK. 
+• Bence Gadanyi 
+• Bence Gadányi 
+• Gadányi Bence"/>
+<meta property="og:image" content="https://bence.codes/static/bence_animoji-2af786ebfcefd0471a58bfe59b40f2a0.png"/>
 
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content="Bence Gadányi's Portfolio" />
-      <meta name="twitter:description" content="Bence Gadányi - Aspiring full-stack software engineer, from Hungary based in the UK. • Bence Gadanyi • Bence Gadányi • Gadányi Bence" />
-      <meta name="twitter:image" content="/static/bence_animoji.png" />
+<meta property="twitter:card" content="summary_large_image"/>
+<meta property="twitter:url" content="https://bence.codes/"/>
+<meta property="twitter:title" content="Bence Gadányi"/>
+<meta property="twitter:description" content="Bence Gadányi - Aspiring full-stack software engineer, from Hungary based in the UK. 
+• Bence Gadanyi 
+• Bence Gadányi 
+• Gadányi Bence"/>
+<meta property="twitter:image" content="https://bence.codes/static/bence_animoji-2af786ebfcefd0471a58bfe59b40f2a0.png"/>
+
 
       <meta name="google-site-verification" content="Ikq1f_qqi7yyulY4SvLERDjTKFMOj0pogf5p9ny-L_w"></meta>
       <link rel="preload" href="/static/chainbridge-8a69c245463eb89c4018f84547cf903a.jpg"/>
