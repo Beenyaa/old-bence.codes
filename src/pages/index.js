d3c19e7af8ae617e18d4bce0c84025fea213390c
Bence Gadányi <29914644+Beenyaa@users.noreply.github.com>
2021-09-16 15:25:58 +0100

    updated work position

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,7 +50,7 @@ const PageConstruct = () => {
           <MiddleAlignContainer>
             <div>
               <p>
-              soon to start @ <b><a class="external-frontpage" href="https://www.netcompany.com" rel="noreferrer" target="_blank">netcompany</a></b>
+              developer/consultant @ <b><a class="external-frontpage" href="https://www.netcompany.com" rel="noreferrer" target="_blank">netcompany</a></b>
               </p>
             </div>
           </MiddleAlignContainer>
